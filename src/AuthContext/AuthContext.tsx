ed3327dd7ac567f59c1a34f8caf6c3214d414bea
Dmytro Shaposhnikov <dimacr137@gmail.com>
2026-03-28 21:36:23 +0100

    feat(auth): persist user session on page reload
    - Added useEffect in AuthContextProvider to check active sessions on mount
    - Secured route rendering by strictly checking data.session for null
    - Handled async effects properly with inner function and void operator

--- src/AuthContext/AuthContext.tsx
+++ src/AuthContext/AuthContext.tsx
@@ -1,4 +1,6 @@
-import {createContext, useContext, useState} from 'react';
+import {createContext, useContext, useEffect, useState} from 'react';
+
+import {supabase} from '../supabase.ts';
 
 interface IAuthContext {
   isAuth: boolean;
@@ -17,6 +19,25 @@ export const AuthContext = createContext<IAuthContext>({
 
 const AuthContextProvider = ({ children }: IWrapperProps) => {
   const [isAuth, setIsAuth] = useState<boolean>(false)
+
+  useEffect(() => {
+      const getUserSession = async () => {
+        try {
+          const {data, error} = await supabase
+          .auth
+          .getSession()
+      if (error) {
+        alert(error.message)
+      } else if (data.session !== null) {
+        setIsAuth(true)
+      }
+        }
+        catch {
+          console.log('Error getting session');
+        }
+      }
+      void getUserSession()
+  }, []);
 
   return (
     <AuthContext.Provider value={{
