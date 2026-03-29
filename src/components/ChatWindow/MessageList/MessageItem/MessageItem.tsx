845887ab2ba98c926e915ec79779fe2c19ba1906
Dmytro Shaposhnikov <dimacr137@gmail.com>
2026-03-29 23:09:46 +0200

    feat(chat): display sender identity for incoming messages
    - Added conditional rendering for sender email
    - Prevented rendering identity for current user's messages to improve UX

--- src/components/ChatWindow/MessageList/MessageItem/MessageItem.tsx
+++ src/components/ChatWindow/MessageList/MessageItem/MessageItem.tsx
@@ -1,3 +1,5 @@
+import './messageItem.scss'
+
 import type {IMessageData} from '../MessageList.tsx';
 
 interface IMessageItemProps {
@@ -9,6 +11,7 @@ const MessageItem = ({messageData, currentUser}: IMessageItemProps) => {
   return (
     <div className={messageData.sender === currentUser ?
       'messages__item messages__item--mine' : 'messages__item messages__item--other'}>
+      {messageData.sender !== currentUser && <span className="messages__sender">{messageData.sender}</span>}
       {messageData.message}
     </div>
   )
